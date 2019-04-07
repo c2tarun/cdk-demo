9f731041980afaa87f84d33044127656020ab171
Tarun K. Mall <c2tarun@hotmail.com>
2019-04-07 11:24:40 -0700

    Updating to version 28

--- lib/random_quote-stack.ts
+++ lib/random_quote-stack.ts
@@ -1,8 +1,10 @@
 import cdk = require("@aws-cdk/cdk");
 import lambda = require("@aws-cdk/aws-lambda");
+import api = require("@aws-cdk/aws-apigateway");
 
 export class RandomQuoteStack extends cdk.Stack {
   public readonly randomQuoteLambda: lambda.Function;
+  public readonly randomQuoteApi: api.LambdaRestApi;
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -10,6 +12,10 @@ export class RandomQuoteStack extends cdk.Stack {
       runtime: lambda.Runtime.NodeJS810,
       handler: "random_quote.handler",
       code: lambda.Code.asset("lambda")
+    });
+
+    this.randomQuoteApi = new api.LambdaRestApi(this, "randomQuoteApi", {
+      handler: this.randomQuoteLambda
     });
   }
 }
