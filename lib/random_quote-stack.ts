5074ff8752619351571fc72bf190fbf5502d5d66
Tarun K. Mall <c2tarun@hotmail.com>
2019-04-07 13:47:56 -0700

    Changing Api gateway Proxy to Resources and adding CORS.

--- lib/random_quote-stack.ts
+++ lib/random_quote-stack.ts
@@ -15,7 +15,59 @@ export class RandomQuoteStack extends cdk.Stack {
     });
 
     this.randomQuoteApi = new api.LambdaRestApi(this, "randomQuoteApi", {
-      handler: this.randomQuoteLambda
+      handler: this.randomQuoteLambda,
+      proxy: false
     });
+
+    const randomQuoteLambdaIntegration = new api.LambdaIntegration(this.randomQuoteLambda);
+    const quoteApiRes = this.randomQuoteApi.root.addResource("quote");
+    quoteApiRes.addMethod("GET", randomQuoteLambdaIntegration, {
+      authorizationType: api.AuthorizationType.None,
+      apiKeyRequired: false
+    });
+    quoteApiRes.addMethod("POST", randomQuoteLambdaIntegration, {
+      authorizationType: api.AuthorizationType.None,
+      apiKeyRequired: false
+    });
+
+    this.addCorsOptions(this.randomQuoteApi.root);
+    this.addCorsOptions(quoteApiRes);
+  }
+
+  addCorsOptions(apiResource: api.IRestApiResource) {
+    apiResource.addMethod(
+      "OPTIONS",
+      new api.MockIntegration({
+        integrationResponses: [
+          {
+            statusCode: "200",
+            responseParameters: {
+              "method.response.header.Access-Control-Allow-Headers":
+                "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
+              "method.response.header.Access-Control-Allow-Origin": "'*'",
+              "method.response.header.Access-Control-Allow-Credentials": "'false'",
+              "method.response.header.Access-Control-Allow-Methods": "'OPTIONS,GET,PUT,POST,DELETE'"
+            }
+          }
+        ],
+        passthroughBehavior: api.PassthroughBehavior.Never,
+        requestTemplates: {
+          "application/json": '{"statusCode": 200}'
+        }
+      }),
+      {
+        methodResponses: [
+          {
+            statusCode: "200",
+            responseParameters: {
+              "method.response.header.Access-Control-Allow-Headers": true,
+              "method.response.header.Access-Control-Allow-Methods": true,
+              "method.response.header.Access-Control-Allow-Credentials": true,
+              "method.response.header.Access-Control-Allow-Origin": true
+            }
+          }
+        ]
+      }
+    );
   }
 }
